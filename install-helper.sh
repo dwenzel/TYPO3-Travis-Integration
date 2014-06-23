a76cf19852feec3b47150ca9199fbe7f202c6bb1
Dirk Wenzel <der-wenzel@gmx.de>
2014-06-24 00:46:02 +0200

    Update install-helper.sh

--- install-helper.sh
+++ install-helper.sh
@@ -85,7 +85,7 @@ function phpCodeSniff {
 	pear install typo3/TYPO3CMS
 	pear config-set preferred_state stable
 	phpenv rehash
-	phpcs -i
+	phpcs --standard=TYPO3CMS $PWD/typo3conf/ext/ajaxmap/
 }
 	
 	
